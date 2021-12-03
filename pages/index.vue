07030d539b34f5c0016ab3ffadc16bc00ebbee1b
Bruno Gonzales <57231813+brunogonzales@users.noreply.github.com>
2021-12-03 16:33:09 -0500

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+
+<template>
+  <div class="flex">
+    <div class="w-10 h-10 bg-red-500 flex items-center justify-center">1</div>
+    <div class="w-10 h-10 bg-blue-500 ml-auto">2</div>
+  </div>
+</template>
+
+
+
